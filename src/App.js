33a6d82c60f3e4dc2f97e8b3517c219a401e23b6
usersunilkumar <imsunilkumar20@gmail.com>
2023-01-25 11:07:37 +0530

    sandeep

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Navbar/>
     </div>
   );
 }
