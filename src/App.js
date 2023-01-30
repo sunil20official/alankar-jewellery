fbabb4834a6e86b2d07c861129a28d9c3f948073
Sandeep Sharma <sks34246@gmail.com>
2023-01-30 15:45:15 +0530

    footer prototype done 3:45

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import HeroSection from './components/heroSection/HeroSection';
 import Navbar from './components/navbar/Navbar';
 import Carousel from './components/carousel/Carousel.jsx';
 import OurCreations from './components/ourcreations/OurCreations';
+import Footer from './components/footer/Footer';
 
 function App() {
 	return (
-    <div style={{minHeight:"400vh"}} className="App">
-      <Navbar />			
+		<div style={{}} className='App'>
+			<Navbar />
 			<HeroSection />
 			<Carousel />
 			<OurCreations />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
