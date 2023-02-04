caa2ca86760ca989e0f5cf430e1410ea555774bc
Sandeep Sharma <43952049+Sandeep2095@users.noreply.github.com>
2023-02-04 12:33:37 +0530

    Update Watch.jsx

--- src/pages/watches/Watch.jsx
+++ src/pages/watches/Watch.jsx
@@ -1,75 +1,58 @@
-import React from "react";
-import "./Watch.css";
-import HeadingComponent from "../../components/shared/headingcomponent/HeadingComponent";
-import ButtonComponent from "../../components/shared/buttonComponent/ButtonComponent";
+import React from 'react';
+import './Watch.css';
+import HeadingComponent from '../../components/shared/headingcomponent/HeadingComponent';
+import ButtonComponent from '../../components/shared/buttonComponent/ButtonComponent';
 
-import image1 from "../../assets/watchs/herolarged_avenue_collection.avif";
-import image2 from "../../assets/creationsImage/Watch3.jpg";
-import image3 from "../../assets/watchs/Watch4.jpg";
+import image1 from '../../assets/watchs/herolarged_avenue_collection.avif';
+import w1 from '../../assets/watchs/Watch1.jpg';
+import w2 from '../../assets/watchs/Watch2.jpg';
+import w3 from '../../assets/watchs/Watch3.jpg';
+import w4 from '../../assets/watchs/Watch4.jpg';
+import w5 from '../../assets/watchs/Watch5.jpg';
+import w6 from '../../assets/watchs/Watch6.jpg';
+import w7 from '../../assets/watchs/Watch7.jpg';
+import w8 from '../../assets/watchs/Watch7.jpg';
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Watch = () => {
-  return (
-    <div className="watch-section-container">
-      <div className="container-top">
-        <img src={image1} style={{ width: "100%" }} alt="" />
-      </div>
-      <div className="div">
-        <HeadingComponent
-          heading={"Watchmaking"}
-          paragraph={
-            "Through its collections, the House of Alankar reinterprets time in the most elegant way."
-          }
-          paragraph2={
-            "Enter a world of pure luxury and know-how to explore our exceptional creations."
-          }
-          marginTop={"0px"}
-        />
-      </div>
-      <div>
-        <div
-          className="container watch-box d-flex gap-4 mb-3"
-          style={{ background: "#fff" }}
-        >
-          <div className="card w-50">
-            <img src={image2} alt="Watch" />
-            <h3>WATCH RADO</h3>
-          </div>
-          <div className="card w-50">
-            <img src={image3} alt="" /> <h3>WATCH RADO</h3>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div
-          className="container watch-box d-flex gap-4 mb-3"
-          style={{ background: "#fff" }}
-        >
-          <div className="card w-50">
-            <img src={image2} alt="Watch" /> <h3>WATCH RADO</h3>
-          </div>
-          <div className="card w-50">
-            <img src={image3} alt="" /> <h3>WATCH RADO</h3>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div
-          className="container watch-box d-flex gap-4 mb-3"
-          style={{ background: "#fff" }}
-        >
-          <div className="card w-50">
-            <img src={image2} alt="Watch" /> <h3>WATCH RADO</h3>
-          </div>
-          <div className="card w-50">
-            <img src={image3} alt="" /> <h3>WATCH RADO</h3>
-          </div>
-        </div>
-      </div>
-      <div className="div">
-        <ButtonComponent />
-      </div>
-    </div>
-  );
+	const watches = [w1, w2, w3, w4, w5, w6, w7, w8];
+	return (
+		<div className='watch-section-container'>
+			<div className='container-top'>
+				<img src={image1} style={{ width: '100%' }} alt='watch-top-image' />
+			</div>
+			<div className='div'>
+				<HeadingComponent
+					heading={'Watchmaking'}
+					paragraph={
+						'Through its collections, the House of Alankar reinterprets time in the most elegant way.'
+					}
+					paragraph2={
+						'Enter a world of pure luxury and know-how to explore our exceptional creations.'
+					}
+					marginTop={'0px'}
+				/>
+			</div>
+			<div className='watch-container'>
+				<div className='watch-grid'>
+					{watches.map((item) => (
+						<div className='item-container'>
+							<LazyLoadImage
+								style={{ width: '100%' }}
+								src={item}
+								effect='blur'
+								alt='collection of pics from alankar store'
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className='div'>
+				<ButtonComponent />
+			</div>
+		</div>
+	);
 };
 
 export default Watch;
