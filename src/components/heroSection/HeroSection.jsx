a82f1cd3e9c60788ac561b6ca90f3ffea1f4f5b8
Sandeep Sharma <sks34246@gmail.com>
2023-02-06 18:55:05 +0530

    video section

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -5,7 +5,15 @@ import VideoBg from '../../assets/videoBg.mp4';
 const HeroSection = () => {
 	return (
 		<div className='heroSection_main'>
-			<video src={VideoBg} autoPlay loop muted></video>
+			<video
+				src={VideoBg}
+				preload='auto'
+				autoPlay
+				loop
+				muted
+				playsinline
+				alt='background video for herosection'
+			></video>
 			<div className='heroSection_content'>
 				<h1>Alankar Jewellars</h1>
 				<p>It will be Your Lucky One</p>
